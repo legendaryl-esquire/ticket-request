2db9e13a8cd3ff35a1c7b2ef61b4e53b86e6eccc
Eronatics-Esquire <aaron.castaneda@equire.com.ph>
2026-03-26 09:17:52 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,11 @@ Route::prefix('admin')->group(function () {
             ]);
         })->name('home');
     });
-    
+    Route::middleware(['auth', 'verified'])->group(function () {
+    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+});    
     Route::middleware('guest')->get('/login', function () {
-        return redirect('/admin');
+        return redirect('/admin', );
     })->name('login');
 
     require __DIR__ . '/settings.php';
